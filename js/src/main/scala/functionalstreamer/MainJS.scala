055f4e1978fec145816c4828c2d6bb0736cdef36
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2017-04-17 10:57:15 +0300

    JS sets the view

--- js/src/main/scala/functionalstreamer/MainJS.scala
+++ js/src/main/scala/functionalstreamer/MainJS.scala
@@ -1,9 +1,11 @@
 package functionalstreamer
 
 import scala.scalajs.js.JSApp
+import org.scalajs.dom.{document, window}
 
 object MainJS extends JSApp {
-  def main(): Unit = {
-    println("Hello World")
+  def main(): Unit = window.onload = { _ =>
+    val placeholder = document.getElementById("body-placeholder")
+    placeholder.innerHTML = "Hello World from JS"
   }
 }
